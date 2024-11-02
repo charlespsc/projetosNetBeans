d49e7bd15f4447d4f623b218874eabfa8b8cf444
Charles Pereira <charlespereira.fln@gmail.com>
2024-11-02 18:15:12 -0300

    OperadoresLógicos

---
+++ OperadoresLogicos/src/operadoreslogicos/OperadoresLogicos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoreslogicos;
+
+/**
+ *
+ * @author charles
+ */
+public class OperadoresLogicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x, y, z;
+        x = 4;
+        y = 7;
+        z = 12;
+        boolean result;
+        
+        result = (x<y && y==z)?true:false; // Podemos usar o OU exclusivo (^) e OU ( || )
+        System.out.println(result);
+        
+    }
+    
+}
